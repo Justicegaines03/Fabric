c24839e1363ce8783ab30c4e3a43d539ebc8ba38
Justice Gaines <justiceg@uoregon.edu>
2025-07-21 19:24:42 -0700

    Added telos aliases in a new .sh file
    Introduces a new aliases.sh file containing shortcuts for common Fabric and Telos commands, pattern invocations, and quick navigation to project directories. These aliases streamline workflow and improve efficiency for project-related tasks.

---
+++ shell/aliases.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Fabric Project Aliases
+# Source this file in your ~/.zshrc with: source "/Users/justicegaines/Library/CloudStorage/OneDrive-UniversityOfOregon/01 Personal/Cybersecurity R&D, LLC/01 Projects/Fabric/shell/aliases.sh"
+
+# Personal TELOS file alias
+alias telos="cat '/Users/justicegaines/Library/CloudStorage/OneDrive-UniversityOfOregon/01 Personal/Cybersecurity R&D, LLC/01 Projects/Telos/personal_telos.md'"
+
+# Fabric pattern shortcuts
+alias fabric-redteam="fabric --pattern t_red_team_thinking"
+alias fabric-analyze="fabric --pattern analyze_paper"
+alias fabric-wisdom="fabric --pattern extract_wisdom"
+alias fabric-summary="fabric --pattern summarize"
+
+# Combined commands for TELOS analysis
+alias telos-redteam="telos | fabric --pattern t_red_team_thinking"
+alias telos-analyze="telos | fabric --pattern analyze_paper"
+alias telos-wisdom="telos | fabric --pattern extract_wisdom"
+
+# Fabric development shortcuts
+alias fabric-patterns="ls '/Users/justicegaines/Library/CloudStorage/OneDrive-UniversityOfOregon/01 Personal/Cybersecurity R&D, LLC/01 Projects/Fabric/data/patterns/'"
+alias fabric-dir="cd '/Users/justicegaines/Library/CloudStorage/OneDrive-UniversityOfOregon/01 Personal/Cybersecurity R&D, LLC/01 Projects/Fabric'"
+
+# Quick navigation
+alias telos-dir="cd '/Users/justicegaines/Library/CloudStorage/OneDrive-UniversityOfOregon/01 Personal/Cybersecurity RD, LLC/01 Projects/Telos'"
